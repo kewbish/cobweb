49ad56fed193575166cae16dcf42cff43d9ddc13
Emilie Ma <kewbish@gmail.com>
2022-12-08 20:45:04 -0800

    Search in body of page as meta-tag fallback

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -9,4 +9,18 @@ if (monetizationTag) {
     message: MONTAG_FOUND,
     options: { address: monetizationTag.content },
   });
+} else {
+  // TODO - replace with more efficient search algorithms
+  const bodyText = document.body.innerText;
+  const monetizationTagGroups = bodyText.match(/COBWEB:(\s)?(?<address>(\w)+)/)
+    ?.groups as { address: string } | null;
+  if (monetizationTagGroups) {
+    console.log(
+      `COBWEB: Monetization Tag Found - ${monetizationTagGroups.address}`
+    );
+    chrome.runtime.sendMessage({
+      message: MONTAG_FOUND,
+      options: { address: monetizationTagGroups.address },
+    });
+  }
 }
