4ea187fb8e30899b111459dcea9a9845ef62ee62
Emilie Ma <kewbish@gmail.com>
2022-12-09 08:33:06 -0800

    Add basic identifier put

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -8,7 +8,20 @@ const worker = {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    return new Response("Hello World!");
+    const params = new URLSearchParams(request.url);
+    const identifier = params.get("identifier");
+    const address = params.get("address");
+    if (identifier && address) {
+      await env.COBWEB_KV.put(identifier, address);
+      const total = await env.COBWEB_KV.get("total");
+      if (total) {
+        await env.COBWEB_KV.put("total", String(parseInt(total) - 1));
+      }
+      return new Response(`Associated ${identifier} to ${address}.`);
+    }
+    return new Response(
+      "Error: could not get `identifier` and `address` params."
+    );
   },
 };
 
