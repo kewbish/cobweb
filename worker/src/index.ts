a8a0005bcab9fc4c9b653dcbf0806de39dd7aeff
Emilie Ma <kewbish@gmail.com>
2022-12-10 08:24:22 -0800

    Add basic authorization gate

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,5 +1,6 @@
 export interface Env {
   COBWEB_KV: KVNamespace;
+  ADMIN_KEY: string;
 }
 
 const worker = {
@@ -8,20 +9,24 @@ const worker = {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const params = new URLSearchParams(request.url);
-    const identifier = params.get("identifier");
-    const address = params.get("address");
-    if (identifier && address) {
-      await env.COBWEB_KV.put(identifier, address);
-      const total = await env.COBWEB_KV.get("total");
-      if (total) {
-        await env.COBWEB_KV.put("total", String(parseInt(total) - 1));
+    if (request.headers.get("Authorization")?.endsWith(` ${env.ADMIN_KEY}`)) {
+      const params = new URLSearchParams(request.url);
+      const identifier = params.get("identifier");
+      const address = params.get("address");
+      if (identifier && address) {
+        await env.COBWEB_KV.put(identifier, address);
+        const total = await env.COBWEB_KV.get("total");
+        if (total) {
+          await env.COBWEB_KV.put("total", String(parseInt(total) - 1));
+        }
+        return new Response(`Associated ${identifier} to ${address}.`);
       }
-      return new Response(`Associated ${identifier} to ${address}.`);
+      return new Response(
+        "Error: could not get `identifier` and `address` params."
+      );
+    } else {
+      return new Response("Error: unauthorized.");
     }
-    return new Response(
-      "Error: could not get `identifier` and `address` params."
-    );
   },
 };
 
