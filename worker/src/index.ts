76ec36f04e9f93233aeb9956202c4b1a21daf175
Emilie Ma <kewbish@gmail.com>
2022-12-20 08:26:24 -0800

    Add CORS headers to worker

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -12,6 +12,9 @@ export interface Env {
 const HEADERS = {
   headers: {
     "content-type": "application/json;charset=UTF-8",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS",
+    "Access-Control-Max-Age": "86400",
   },
 };
 
